2f4217481b02c71faa62ff615fb12054b562e3a4
Romeo <romeolacerdafarias@gmail.com>
2025-12-10 17:40:47 -0300

    feat: add ai chat

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,8 +1,10 @@
 "use client";
 
+import { MenuIcon, MessageCircleIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import SidebarMenu from "./sidebar-menu";
 import { Button } from "./ui/button";
-import { MenuIcon } from "lucide-react";
 import {
   Sheet,
   SheetContent,
@@ -10,13 +12,17 @@ import {
   SheetTitle,
   SheetTrigger,
 } from "./ui/sheet";
-import SidebarMenu from "./sidebar-menu";
 
 const Header = () => {
   return (
     <header className="flex items-center justify-between bg-white px-5 py-6">
       <Image src="/logo.svg" alt="Aparatus" width={100} height={26.09} />
       <div className="flex items-center gap-2">
+        <Button variant="outline" size="icon" asChild>
+          <Link href="/chat">
+            <MessageCircleIcon />
+          </Link>
+        </Button>
         <Sheet>
           <SheetTrigger asChild>
             <Button variant="outline" size="icon">
---
+++ app/api/chat/route.ts
@@ -0,0 +1,13 @@
+import { google } from "@ai-sdk/google";
+import { convertToModelMessages, streamText } from "ai";
+
+export const POST = async (request: Request) => {
+  const { messages } = await request.json();
+  
+  const result = streamText({
+    model: google("gemini-2.0-flash"),
+    system: `Você é o Aparatus AI, um assistente virtual de agendamento de barbearias`,
+    messages: convertToModelMessages(messages),
+  });
+  return result.toUIMessageStreamResponse();
+};
